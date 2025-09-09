97f04e82e051da5408a78eeb1c10bcd134580343
CodeWithBry <bryanagustinpajarillaga@gmaill.com>
2025-09-09 17:34:57 +0800

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,13 @@
-// This file must be plain JS, not TS, because service workers don't support ES modules in all browsers
 importScripts('https://www.gstatic.com/firebasejs/10.12.4/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.4/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyAKcCWk_hS2I5PRmN6C0DqPc7PIyAsXQxM",
-    authDomain: "activity-60d78.firebaseapp.com",
-    projectId: "activity-60d78",
-    storageBucket: "activity-60d78.firebasestorage.app",
-    messagingSenderId: "784214109781",
-    appId: "1:784214109781:web:cfcb864780520b4e1a2edc",
-    measurementId: "G-77F988Z08Z"
+    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
 });
 
 const messaging = firebase.messaging();
